6c6a963742c417de5adac5e08b82f31b3ea51c85
Rohith Nair <rohith.nair@neutrinos.co>
2024-04-02 18:20:13 +0530

    css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Chatflow } from "./Modules/Chatflow";
+import Editor from "./components/editor/editor.component";
 import Navbar from "./components/navbar/navbar.component";
 
 function App() {
+    const chatFlow = new Chatflow();
     return (
         <>
             <Navbar></Navbar>
+            <Editor chatFlow={chatFlow}></Editor>
         </>
     );
 }
