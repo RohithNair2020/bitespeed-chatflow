1def81dc61f5e4533fdfd307d5aadf6fb4479dde
Rohith Nair <rohith.nair@neutrinos.co>
2024-04-02 18:19:34 +0530

    Navbar component added

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,6 +1,6 @@
-import './navbar.style.module.css';
+import styles from './navbar.styles.module.css';
 function Navbar() {
-    return <div>Navbar</div>;
+    return <div className={styles.navbarContainer}>Navbar</div>;
 }
 
 export default Navbar;
